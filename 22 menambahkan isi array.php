b5320c2ecada0530c8af74e45142cb452818a096
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-07 16:52:30 +0700

    array asosiatif

--- 22 menambahkan isi array.php
+++ 22 menambahkan isi array.php
@@ -16,5 +16,22 @@ $hobi[] = "Olahraga";
 foreach($hobi as $hobiku){
     echo $hobiku."<br>";
 }
+?>
+<?php
+// membuat array
+$user = [
+    "lia",
+    "lala",
+    "syarifahnaylarzky"
+];
+
+// mengisi array pada indek ke-1 ("muhar")
+$user[1] = "Nayla";
+
+// mencetak isi array
+echo "<pre>";
+print_r($user);
+echo "</pre>";
+
 print_r("<p><strong>by :syarifah nayla rizky");
 ?>
---
+++ 23 Array Asosiatif.php
@@ -0,0 +1,22 @@
+<?php
+// membuat array asosiatif
+$artikel = [
+    "judul" => "Belajar Pemrograman PHP",
+    "penulis" => "syarifahnaylarizky",
+    "view" => 227
+];
+
+// mencetak isi array assosiatif
+echo "<h2>".$artikel["judul"]."</h2>";
+echo "<p>oleh: ".$artikel["penulis"]."</p>";
+echo "<p>View: ".$artikel["view"]."</p>";
+
+echo "<hr>";
+
+$email["subjek"] = "Apa Kabar?";
+$email["pengirim"] = "lia@naylaksp2024.com";
+$email["isi"] = "Apa kabar? sudah lama tidak berjumpa";
+
+echo "<pre>";
+print_r($email);
+echo "</pre>";
