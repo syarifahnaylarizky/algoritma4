2012f882bdaceb6a60fac86118c279702dc0b310
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-07 16:41:13 +0700

    isi array

--- 19 menampilkan isi array.php
+++ 19 menampilkan isi array.php
@@ -1,18 +1,10 @@
 <?php
 // membuat array
 $barang = ["Buku Tulis", "Penghapus", "Spidol"];
-
+	
 // menampilkan isi array
 echo $barang[0]."<br>";
 echo $barang[1]."<br>";
 echo $barang[2]."<br>";
-echo"<hr>";
-
-// membuat array
-$barang = ["Buku Tulis", "Penghapus", "Spidol"];
-// menampilkan isi array dengan perulangan for
-for($i=0; $i < count($barang); $i++){
-    echo $barang[$i]."<br>";
-}
 print_r("<p><strong>by :syarifah nayla rizky");
 ?>
