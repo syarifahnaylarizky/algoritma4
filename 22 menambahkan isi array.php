f02c62652479305dabe74cb9b30c982637bf97d8
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-07 16:45:12 +0700

    menambahkan isi array

---
+++ 22 menambahkan isi array.php
@@ -0,0 +1,20 @@
+<?php
+// membuat array
+$hobi = [
+    "Membaca",
+    "Menulis",
+    "Ngeblog"
+];
+
+// menambahkan isi pada idenks ke-3
+$hobi[3] = "Coding";
+
+// menambahkan isi pada indeks terakhir
+$hobi[] = "Olahraga";
+
+// cetak array dengan perulangan
+foreach($hobi as $hobiku){
+    echo $hobiku."<br>";
+}
+print_r("<p><strong>by :syarifah nayla rizky");
+?>
