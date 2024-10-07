5e4071771fa33dd0f80fbb773c77480b01356265
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-07 17:04:34 +0700

    array 1 dimensi

---
+++ 26 array 1 dimensi.php
@@ -0,0 +1,5 @@
+<?php
+$test=array("Index pertama","Index Kedua","Index ketiga");
+echo "Hasil array : 0." . $test[0] . ", 1." . $test[1] . " dan 2." . $test[2] . ".";
+print_r("<p><strong>by :syarifah nayla rizky");
+?>
