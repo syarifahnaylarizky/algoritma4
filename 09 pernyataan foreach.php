6250c97082a704e115521e42864fa90b06b904e5
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-07 16:08:43 +0700

    membuat pernyataan forech

--- 09 pernyataan foreach.php
+++ 09 pernyataan foreach.php
@@ -3,5 +3,5 @@ $warna = array('merah', 'hijau', 'biru');
 foreach ($warna as $w) {
   echo "Warna: $w";
 }	
-print_r("<p><strong>by :syarifah nayla rizkyy");
+print_r("<p><strong>by :syarifah nayla rizky");
 ?>
