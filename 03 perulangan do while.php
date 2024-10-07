db5c21cd93e693bbd34aa248d40e691f91637f56
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-07 11:32:34 +0700

    membuat perulangan do while

--- 03 perulangan do while.php
+++ 03 perulangan do while.php
@@ -6,5 +6,5 @@ do {
     echo "<p>ini adalah perulangan ke-$ulangi</p>";
     $ulangi--;
 } while ($ulangi > 0);
-print_r("<p><strong>by :syarifah nayla rizkyy");
+print_r("<p><strong>by :syarifah naila rizky");
 ?>
