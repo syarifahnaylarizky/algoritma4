b33a5e256e653afc003af9cfac269c44ac3b4665
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-08 11:15:46 +0700

    menambahkan isi array

--- 23 Array Asosiatif.php
+++ 23 Array Asosiatif.php
@@ -13,16 +13,11 @@ echo "<p>View: ".$artikel["view"]."</p>";
 
 echo "<hr>";
 
+
 $email["subjek"] = "Apa Kabar?";
-$email["pengirim"] = "lia@naylaksp2024.com";
+$email["pengirim"] = "nayla@naylaksp.com";
 $email["isi"] = "Apa kabar? sudah lama tidak berjumpa";
 
 echo "<pre>";
 print_r($email);
 echo "</pre>";
-
-echo "<hr>";
-$nama_variabel=array("nama1"=>"70","nama2"=>"67","nama3"=>"89");
-  echo "hasil dari program " . $nama_variabel['nama1'] . " nilai.";
-  print_r("<p><strong>by :syarifah nayla rizky");
-  ?>
