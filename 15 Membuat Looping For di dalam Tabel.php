4114652cfd744fff78dcb145b31dc5d656e6e0cf
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-08 10:36:48 +0700

    membuat looping for di dalam tabel

--- 15 Membuat Looping For di dalam Tabel.php
+++ 15 Membuat Looping For di dalam Tabel.php
@@ -1,23 +1,7 @@
 <!DOCTYPE html>
 <html>
-<head>
+<head>	
 	<title>Cara Membuat Looping For di dalam Tabel</title>
-    <style>
-	table{
-        width:300px; 
-        text-align:center; 
-        margin:auto;
-    }
-	table th { 
-        background-color: #95a5a6; 
-    }
-	h2 {
-        text-align:center; 
-        font-style:italic; 
-        font-weight:bold;
-        }
-</style>
-
 </head>
 <body>		
 	<h2>Cara Membuat Looping For di dalam Tabel</h2> 
@@ -37,3 +21,28 @@
 	</form>
 </body>
 </html>
+<style>
+	table{width:300px; text-align:center; margin:auto;}
+	table th { background-color: #95a5a6; }
+	h2 {text-align:center; font-style:italic; font-weight:bold;}
+</style>
+<form>
+		<table border="1" cellspacing="0">
+			<tr>
+<th>NO</th>
+<th>BUAH</th>
+<th>SAYUR</th>
+			</tr>
+ 	
+<?php  for ($no = 1, $i=10, $a=100; $i<=100, $a<=1000  ; $i+=10, $a+=100) { ?>
+ 
+			<tr>
+<td><?php echo $no; ?></td>
+<td><?php echo $i; ?></td>
+<td><?php echo $a; ?></td>
+			</tr>
+ 
+		<?php $no++; } ?>
+
+		</table>
+	</form>
